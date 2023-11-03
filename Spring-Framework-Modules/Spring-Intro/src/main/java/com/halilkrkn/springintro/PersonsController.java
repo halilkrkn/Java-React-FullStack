67215fb886fe946e218f5ec690e1a360508f8f0f
halilkrkn <Halilkrkn000@gmail.com>
2023-11-03 12:10:40 +0300

    Spring Person Crud

--- Spring-Framework-Modules/Spring-Intro/src/main/java/com/halilkrkn/springintro/PersonsController.java
+++ Spring-Framework-Modules/Spring-Intro/src/main/java/com/halilkrkn/springintro/PersonsController.java
@@ -10,22 +10,53 @@ import java.util.List;
 public class PersonsController {
     // In-Memory DB
     private List<Person> personList = new ArrayList<>();
+
     @GetMapping
-    public List<Person> getAll(){
+    public List<Person> getAll() {
         return personList;
     }
 
     @GetMapping("{id}")
-    public void getById(@PathVariable int id){
+    public Person getById(@PathVariable int id) {
         // Araştırma konusu
         // stream API
-
+        Person person = personList
+                .stream()
+                .filter(p -> p.id == id)
+                .findFirst()
+                .orElseThrow();
+        return person;
     }
 
     @PostMapping
-    public String add(@RequestBody Person person)
-    {
+    public String add(@RequestBody Person person) {
         personList.add(person);
         return "Eklendi";
+    }
+
+    @PutMapping()
+    public String update(@RequestBody Person person) {
+        // veritabanındaki veriyi bul
+        // VERİTABANINDA İLGİLİ VERİ BULUNMAK ZORUNDA
+        // güncellenmek istenen alanı güncelle
+        Person personToUpdate = personList
+                .stream()
+                .filter(p -> p.id == person.id)
+                .findFirst()
+                .orElseThrow(); // throw Exception
+        // böyle bi değer var
+        personToUpdate.name = person.name;
+        personToUpdate.surname = person.surname;
+        return "Güncellendi";
+    }
+
+    @DeleteMapping("{id}")
+    public String delete(@PathVariable int id) {
+        Person personToDelete = personList.stream()
+                .filter(p -> p.id == id)
+                .findFirst()
+                .orElseThrow();
+        personList.remove(personToDelete);
+        return "Silindi";
     }
 }
