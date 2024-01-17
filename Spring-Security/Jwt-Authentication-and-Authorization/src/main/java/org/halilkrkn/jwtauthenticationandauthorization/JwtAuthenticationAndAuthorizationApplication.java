f421ed718fc62efe5136b4f1760cf908f84958ab
halilkrkn <halilkrkn000@gmail.com>
2024-01-17 12:40:58 +0300

    Initialize Spring Security - JWT and Authentication-Authorization

---
+++ Spring-Security/Jwt-Authentication-and-Authorization/src/main/java/org/halilkrkn/jwtauthenticationandauthorization/JwtAuthenticationAndAuthorizationApplication.java
@@ -0,0 +1,13 @@
+package org.halilkrkn.jwtauthenticationandauthorization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtAuthenticationAndAuthorizationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JwtAuthenticationAndAuthorizationApplication.class, args);
+    }
+
+}
